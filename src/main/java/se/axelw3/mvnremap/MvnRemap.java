52d602dfa9d8854b272a6c2f37d844d0cf9b22f2
axelw3 <axelw3@outlook.com>
2026-02-06 12:36:12 +0100

    Support fetching input files via http(s) + add parameter for mapping file

--- src/main/java/se/axelw3/mvnremap/MvnRemap.java
+++ src/main/java/se/axelw3/mvnremap/MvnRemap.java
@@ -1,6 +1,9 @@
 package se.axelw3.mvnremap;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
@@ -14,7 +17,6 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
 
 import net.fabricmc.tinyremapper.ConsoleLogger;
 import net.fabricmc.tinyremapper.NonClassCopyMode;
@@ -23,11 +25,12 @@ import net.fabricmc.tinyremapper.TinyRemapper;
 import net.fabricmc.tinyremapper.TinyRemapper.LinkedMethodPropagation;
 import net.fabricmc.tinyremapper.TinyUtils;
 
-@Mojo(name = "remap", defaultPhase = LifecyclePhase.COMPILE)
+@Mojo(name = "remap", defaultPhase = LifecyclePhase.VALIDATE)
 public class MvnRemap extends AbstractMojo{
-
+/*
     @Parameter(defaultValue = "${project}", readonly = true, required = true)
     private MavenProject project;
+*/
 
     @Parameter(property = "inputFile", required = true)
     private String inputFile;
@@ -35,25 +38,30 @@ public class MvnRemap extends AbstractMojo{
     @Parameter(property = "outputFile", required = true)
     private String outputFile;
 
+    @Parameter(property = "mappingsFile", required = true)
+    private String mappingsFile;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        System.out.println("Detta fungerar (" + project.getArtifactId() + ")!!!");
-
         // 1) remappa obfuskerad --> Fabric "intermediary mappings" (class_xxxx, etc.)
-        final String mappingsFile = "1.21.1.tiny";
         final String fromNamespace = "official";
         final String toNamespace = "intermediary";
 
         ConsoleLogger logger = new ConsoleLogger();
 
-        final Path input = Paths.get(inputFile);
+        Path output = Paths.get(outputFile);
+        if(output.toFile().exists()){
+            logger.info("Skipping remap (target file already exists).");
+            return;
+        }
+
+        final Path input = fetchIfAppropriate(inputFile, "input.jar", logger);
         if (!Files.isReadable(input)) {
             logger.error("Couldn't read " + input + ".");
             System.exit(1);
         }
 
-        Path output = Paths.get(outputFile);
-        Path mappings = Paths.get(mappingsFile);
+        Path mappings = fetchIfAppropriate(mappingsFile, "mappings.tiny", logger);
         if (!Files.isReadable(mappings) || Files.isDirectory(mappings, new LinkOption[0])) {
             logger.error("Couldn't read mappings " + mappings + ".");
             System.exit(1);
@@ -92,5 +100,25 @@ public class MvnRemap extends AbstractMojo{
         }
 
         logger.info("Remapped jar archive in %.2fms.", (double) (System.nanoTime() - startTime) / 1e6d);
+    }
+
+    @SuppressWarnings("ConvertToTryWithResources")
+    private static Path fetchIfAppropriate(String srcPath, String targetFileName, ConsoleLogger logger){
+        if(srcPath.startsWith("http://") || srcPath.startsWith("https://")){
+            final Path targetFile = Paths.get(targetFileName);
+            try(FileOutputStream fos = new FileOutputStream(targetFile.toFile())){
+                URL url = new URL(srcPath);
+                ReadableByteChannel rbc = java.nio.channels.Channels.newChannel(url.openStream());
+                fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+                rbc.close();
+            }catch(IOException err){
+                logger.error("Couldn't fetch file from url.");
+                System.exit(1);
+            }
+
+            return targetFile;
+        }
+
+        return Paths.get(srcPath);
     }
 }
